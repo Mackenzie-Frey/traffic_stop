c4d524ee61151e93e89000bc38b6c92b80fe0560
Mackenzie-Frey <mackenziefrey1@gmail.com>
2018-11-03 04:37:44 -0600

    Add files and tests for vehicle

---
+++ test/vehicle_test.rb
@@ -0,0 +1,46 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'pry'
+require './lib/driver'
+require './lib/vehicle'
+
+class VehicleTest < Minitest::Test
+
+  def test_it_exists
+    vehicle = Vehicle.new("2001", "Honda", "Civic")
+    assert_instance_of Vehicle, vehicle
+  end
+
+  def test_it_has_a_attributes
+    vehicle = Vehicle.new("2001", "Honda", "Civic")
+    assert_equal "2001", vehicle.year
+    assert_equal "Honda", vehicle.make
+    assert_equal "Civic", vehicle.model
+  end
+
+  def test_it_has_a_nil_for_default_driver
+    vehicle = Vehicle.new("2001", "Honda", "Civic")
+    assert_equal nil, vehicle.driver
+    # will the above also work as assert nil, vehicle.driver
+  end
+
+  def test_it_can_add_drivers
+    vehicle = Vehicle.new("2001", "Honda", "Civic")
+    driver = Driver.new("Bob")
+    vehicle.add_driver(driver)
+    assert_equal driver, vehicle.driver
+  end
+
+  def test_it_can_speed
+    vehicle = Vehicle.new("2001", "Honda", "Civic")
+    assert_equal false, vehicle.speeding?
+    # try the above as refute vehicle.speeding?
+    vehicle.speed
+    assert_equal false, vehicle.speeding?
+    # try the above as assert, vehicle.speeding?
+    assert_equal true, vehicle.speeding?
+  end
+end
+
+# go back through interaction pattern... to confirm correct...
+# aka copy and paste into pry
